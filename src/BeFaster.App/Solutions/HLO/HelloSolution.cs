b47dfe427301930375549387f1c61f7962eacec1
sbx_user1059 <sbx_user1059@ip-10-128-162-78.eu-west-2.compute.internal>
2018-12-11 17:22:44 +0000

    Tue Dec 11 17:22:44 UTC 2018

--- src/BeFaster.App/Solutions/HLO/HelloSolution.cs
+++ src/BeFaster.App/Solutions/HLO/HelloSolution.cs
@@ -6,7 +6,11 @@ namespace BeFaster.App.Solutions.HLO
     {
         public static string Hello(string friendName)
         {
-            throw new SolutionNotImplementedException();
+            //- param[0] = a String. Ignore for now.
+            //-@return = a String containing a message
+
+            return "Hello world";
         }
     }
 }
+
