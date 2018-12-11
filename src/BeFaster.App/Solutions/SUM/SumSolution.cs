68afcaf958f22a2d662d91feb9a4dd7a22bc9b4b
sbx_user1059 <sbx_user1059@ip-10-128-162-78.eu-west-2.compute.internal>
2018-12-11 17:15:50 +0000

    Tue Dec 11 17:15:50 UTC 2018

--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -1,4 +1,5 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿using System.Linq;
+using BeFaster.Runner.Exceptions;
 
 namespace BeFaster.App.Solutions.SUM
 {
@@ -6,7 +7,18 @@ namespace BeFaster.App.Solutions.SUM
     {
         public static int Sum(int x, int y)
         {
-            throw new SolutionNotImplementedException();
+            //- param[0] = a positive integer between 0-100
+            // -param[1] = a positive integer between 0 - 100
+            //- @return = an Integer representing the sum of the two numbers
+
+            if (Enumerable.Range(0, 101).Contains(x)
+                && Enumerable.Range(0, 101).Contains(y))
+            {
+                return x + y;
+            }
+
+            return -1;
         }
     }
 }
+
