2da2a6996a969bc7f93cec5736105a8e6bd5bcab
sbx_user1060 <sbx_user1060@ip-10-128-47-77.eu-west-2.compute.internal>
2018-12-12 11:57:44 +0000

    Wed Dec 12 11:57:44 UTC 2018

---
+++ src/BeFaster.App/Solutions/CHK/DiscountedProduct.cs
@@ -0,0 +1,8 @@
+﻿namespace BeFaster.App.Solutions.CHK
+{
+    public class DiscountedProduct
+    {
+        public int ProductQuantity { get; set; }
+        public int Discount { get; set; }
+    }
+}
