62275f80a83792775b367cf0296589e96b130ae3
sbx_user1060 <sbx_user1060@ip-10-128-47-77.eu-west-2.compute.internal>
2018-12-12 12:00:44 +0000

    Wed Dec 12 12:00:44 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
+        public static Dictionary<string, DiscountedProduct> DiscountedProducts() => new Dictionary<string, DiscountedProduct>
+            {
+                {
+                    "A", new DiscountedProduct
+                        {ProductQuantity = 3, Discount = 20}
+                },
+                {
+                    "B", new DiscountedProduct
+                        {ProductQuantity = 3, Discount = 20}
+                },
+
+            };
         public static int Checkout(string skus)
         {
             throw new NotImplementedException();
         }
     }
 }
+
