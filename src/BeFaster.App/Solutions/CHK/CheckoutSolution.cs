a25f8320a62935b15b891ee7bf8f9d5b22d9d429
sbx_user1059 <sbx_user1059@ip-10-129-64-2.eu-west-2.compute.internal>
2018-12-12 18:10:04 +0000

    Wed Dec 12 18:10:04 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -6,24 +6,16 @@ namespace BeFaster.App.Solutions.CHK
     public static class CheckoutSolution
     {
         public static Dictionary<char, DiscountedProduct> DiscountedProducts => new Dictionary<char, DiscountedProduct>
+        {
             {
-                {
-                    'A', new DiscountedProduct
-                        {ProductQuantity = 3, Discount = 20}
-                },
-                {
-                    'B', new DiscountedProduct
-                        {ProductQuantity = 2, Discount = 15}
-                }
-            };
-
-        //public static Dictionary<char, int> CountProducts => new Dictionary<char, int>
-        //{
-        //    {'A', 0 },
-        //    {'B', 0 },
-        //    {'C', 0 },
-        //    {'D', 0 }
-        //};
+                'A', new DiscountedProduct
+                    {ProductQuantity = 3, Discount = 20}
+            },
+            {
+                'B', new DiscountedProduct
+                    {ProductQuantity = 2, Discount = 15}
+            }
+        };
 
         public static Dictionary<char, int> Prices => new Dictionary<char, int>
         {
@@ -32,16 +24,6 @@ namespace BeFaster.App.Solutions.CHK
             {'C', 20 },
             {'D', 15 }
         };
-
-        public static bool IsProductDiscounted(char skus)
-        {
-            return DiscountedProducts.ContainsKey(skus);
-        }
-
-        //public static bool IsProductAmountEnoughToGetDiscount(char skus)
-        //{
-        //    return CountProducts[skus] == DiscountedProducts[skus].ProductQuantity;
-        //}
 
         public static int GetProduct(string skus)
         {
