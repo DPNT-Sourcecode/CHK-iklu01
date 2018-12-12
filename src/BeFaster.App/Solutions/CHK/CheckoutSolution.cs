80294c2e478c17f3e160f6d057dea518dfdafae5
sbx_user1060 <sbx_user1060@ip-10-128-47-77.eu-west-2.compute.internal>
2018-12-12 13:05:42 +0000

    Wed Dec 12 13:05:42 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -7,66 +7,64 @@ namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
-        //public static Dictionary<string, DiscountedProduct> DiscountedProducts => new Dictionary<string, DiscountedProduct>
-        //    {
-        //        {
-        //            "A", new DiscountedProduct
-        //                {ProductQuantity = 3, Discount = 20}
-        //        },
-        //        {
-        //            "B", new DiscountedProduct
-        //                {ProductQuantity = 2, Discount = 15}
-        //        }
-        //    };
+        public static Dictionary<string, DiscountedProduct> DiscountedProducts => new Dictionary<string, DiscountedProduct>
+            {
+                {
+                    "A", new DiscountedProduct
+                        {ProductQuantity = 3, Discount = 20}
+                },
+                {
+                    "B", new DiscountedProduct
+                        {ProductQuantity = 2, Discount = 15}
+                }
+            };
 
-        //public static Dictionary<string, int> CountProducts => new Dictionary<string, int>
-        //{
-        //    {"A", 0 },
-        //    {"B", 0 },
-        //    {"C", 0 },
-        //    {"D", 0 }
-        //};
+        public static Dictionary<string, int> CountProducts => new Dictionary<string, int>
+        {
+            {"A", 0 },
+            {"B", 0 },
+            {"C", 0 },
+            {"D", 0 }
+        };
 
-        //public static Dictionary<string, int> Prices => new Dictionary<string, int>
-        //{
-        //    {"A", 50 },
-        //    {"B", 30 },
-        //    {"C", 20 },
-        //    {"D", 15 }
-        //};
+        public static Dictionary<string, int> Prices => new Dictionary<string, int>
+        {
+            {"A", 50 },
+            {"B", 30 },
+            {"C", 20 },
+            {"D", 15 }
+        };
 
-        //public static bool IsProductDiscounted(string skus)
-        //{
-        //    return DiscountedProducts.ContainsKey(skus);
-        //}
+        public static bool IsProductDiscounted(string skus)
+        {
+            return DiscountedProducts.ContainsKey(skus);
+        }
 
-        //public static bool IsProductAmountEnoughToGetDiscount(string skus)
-        //{
-        //    return CountProducts[skus] == DiscountedProducts[skus].ProductQuantity;
-        //}
+        public static bool IsProductAmountEnoughToGetDiscount(string skus)
+        {
+            return CountProducts[skus] == DiscountedProducts[skus].ProductQuantity;
+        }
 
-        //public static int GetProduct(string skus)
-        //{
-        //    var priceToPay = 0;
-        //    CountProducts[skus]++;
-        //    priceToPay += Prices[skus];
-        //    if (IsProductDiscounted(skus)
-        //        && IsProductAmountEnoughToGetDiscount(skus))
-        //    {
-        //        priceToPay -= DiscountedProducts[skus].Discount;
-        //    }
+        public static int GetProduct(string skus)
+        {
+            var priceToPay = 0;
+            CountProducts[skus]++;
+            priceToPay += Prices[skus];
+            if (IsProductDiscounted(skus)
+                && IsProductAmountEnoughToGetDiscount(skus))
+            {
+                priceToPay -= DiscountedProducts[skus].Discount;
+            }
 
-        //    if (!Regex.IsMatch(skus, @"^[a-zA-Z]+$"))
-        //    {
-        //        priceToPay = -1;
-        //    }
-        //    return priceToPay;
-        //}
+            if (!Regex.IsMatch(skus, @"^[a-zA-Z]+$"))
+            {
+                priceToPay = -1;
+            }
+            return priceToPay;
+        }
         public static int Checkout(string skus)
         {
-            //return GetProduct(skus);
-            //throw new NotImplementedException();
-            return 0;
+            return GetProduct(skus);
         }
     }
 }
