581cbf30a5d4b40cf81d639adfb8aca6c43eff09
sbx_user1060 <sbx_user1060@ip-10-128-47-77.eu-west-2.compute.internal>
2018-12-12 11:54:46 +0000

    Wed Dec 12 11:54:46 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,4 +1,4 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿using System;
 
 namespace BeFaster.App.Solutions.CHK
 {
@@ -6,17 +6,7 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int Checkout(string skus)
         {
-            switch (skus)
-            {
-                case "A": return 50;
-                case "B": return 30;
-                case "C": return 20;
-                case "D": return 15;
-                case "AAA": return 130;
-                case "BB": return 45;
-                default: return -1;
-            }
+            throw new NotImplementedException();
         }
     }
 }
-
