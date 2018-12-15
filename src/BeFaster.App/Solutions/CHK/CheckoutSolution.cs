7c7c5dc29ce1aba9096f8ef5d45e8445207ebbbd
sbx_user1059 <sbx_user1059@ip-10-129-167-116.eu-west-2.compute.internal>
2018-12-15 13:46:32 +0000

    Sat Dec 15 13:46:32 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -77,7 +77,8 @@ namespace BeFaster.App.Solutions.CHK
                 {
                     if (counts[c] > 4 
                         && countProducts[c] % 5 == 0
-                        || counts[c] == 9 
+                        || counts[c] == 9
+                        && countProducts[c] % 5 == 4
                         || counts[c] > 8 
                         && countProducts[c] % 5 == 0)
                     {
