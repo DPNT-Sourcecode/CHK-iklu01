7803cb12c70979e282f7e294428d862531614d1f
sbx_user1062 <sbx_user1062@ip-10-129-84-248.eu-west-2.compute.internal>
2018-12-15 20:01:00 +0000

    Sat Dec 15 20:01:00 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,28 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Security.Policy;
 using System.Text.RegularExpressions;
 
 namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
-        //public static Dictionary<char, DiscountedProduct> DiscountedProducts => new Dictionary<char, DiscountedProduct>
-        //{
-        //    {
-        //        'A', new DiscountedProduct
-        //            {ProductQuantity = 3, Discount = 20}
-        //    },
-        //    {
-        //        'B', new DiscountedProduct
-        //            {ProductQuantity = 2, Discount = 15}
-        //    },
-        //    {
-        //        'E', new DiscountedProduct
-        //            {ProductQuantity = 2, Discount = 30}
-        //    }
-        //};
-
         public static Dictionary<char, int> Prices => new Dictionary<char, int>
         {
             {'A', 50 },
@@ -32,11 +15,8 @@ namespace BeFaster.App.Solutions.CHK
             {'E', 40 }
         };
 
-        public static Dictionary<char, int> CountProducts(this string skus)
-        {
-            return skus.GroupBy(c => c)
+        public static Dictionary<char, int> CountProducts(this string skus) => skus.GroupBy(c => c)
                 .ToDictionary(group => group.Key, group => group.Count());
-        }
 
         public static int GetProduct(string skus)
         {
@@ -44,7 +24,7 @@ namespace BeFaster.App.Solutions.CHK
             var priceToPay = 0;
             var arr = skus.ToCharArray(0, skus.Length);
 
-            var countProducts = new Dictionary<char, int>
+            var increaseProductNumber = new Dictionary<char, int>
             {
                 {'A', 0 },
                 {'B', 0 },
@@ -55,36 +35,52 @@ namespace BeFaster.App.Solutions.CHK
 
             foreach (var c in arr)
             {
-                countProducts[c]++;
+                increaseProductNumber[c]++;
                 priceToPay += Prices[c];
             }
 
             foreach (var count in counts)
             {
-                if (count.Key == 'A')
+                if (count.Key == 'A' && count.Value > 2)
                 {
-                    if (count.Value % 3 == 0
-                        || count.Value % 3 == 1)
+                    if (count.Value < 6 && count.Value % 3 == 0
+                        || count.Value < 6 && count.Value % 3 == 1)
                     {
                         priceToPay -= 20;
                     }
 
                     if (count.Value % 5 == 0
-                        || count.Value % 5 == 1)
+                        || count.Value % 5 == 1
+                        || count.Value % 5 == 2)
                     {
-                        priceToPay -= 50;
+                        priceToPay -= 50 * (count.Value / 5);
+                    }
+
+                    if (count.Value % 8 == 0
+                        || count.Value % 8 == 1)
+                    {
+                        priceToPay -= 70;
                     }
                 }
 
-                if (count.Key == 'B')
+                if (count.Key == 'B' && count.Value > 1)
                 {
-                    if (count.Value % 2 == 0)
+                    if (count.Value % 2 == 0
+                        || count.Value % 2 == 1)
                     {
                         priceToPay -= 15;
                     }
                 }
 
-                if (count.Key == 'E')
+                if (count.Key == 'E' && count.Value > 1)
+                {
+                    if (priceToPay % 110 <= 110)
+                    {
+                        priceToPay -= 30;
+                    }
+                }
+
+                if (count.Key == 'E' && count.Value > 2)
                 {
                     if (count.Value % 2 == 0)
                     {
@@ -92,79 +88,6 @@ namespace BeFaster.App.Solutions.CHK
                     }
                 }
             }
-
-            //return priceToPay;
-
-            //foreach (var c in arr)
-            //{
-            //    countProducts[c]++;
-            //    priceToPay += Prices[c];
-
-
-
-            //    if (countProducts[c] % 3 == 0)
-            //    {
-            //        priceToPay -= 20;
-            //    }
-
-            //    if (countProducts[c] % 5 == 0)
-            //    {
-            //        priceToPay -= 50;
-            //    }
-
-            //    if (discountedProducts.ContainsKey(c))
-            //    {
-            //        if (counts[c] > 4
-            //            && countProducts[c] % 5 == 0
-            //            || counts[c] > 8
-            //            && countProducts[c] % 5 == 0
-            //            || counts[c] > 8
-            //            && countProducts[c] % 5 == 1)
-            //        {
-            //            priceToPay -= 50;
-            //        }
-
-            //        if (counts[c] <= 4
-            //            && countProducts[c] % 3 == 0
-            //            || counts[c] == 8
-            //            && countProducts[c] % 8 == 0)
-            //        {
-            //            priceToPay -= discountedProducts[c].Discount;
-            //        }
-
-            //        if (counts[c] >= 5 && counts[c] <= 7
-            //            && countProducts[c] % 5 == 0
-            //            )
-            //        {
-            //            priceToPay -= 50;
-            //        }
-
-            //        if (counts[c] <= 4
-            //            && countProducts[c] % 3 == 0
-            //            )
-            //        {
-            //            priceToPay -= discountedProducts[c].Discount;
-            //        }
-
-            //        if (counts[c] >= 8 && counts[c] < 10
-            //            && countProducts[c] % 5 == 0
-            //            || counts[c] >= 8 && counts[c] < 10
-            //            && countProducts[c] % 3 == 0
-            //        )
-            //        {
-            //            priceToPay -= 70;
-            //        }
-
-            //        if (counts[c] >= 8 && counts[c] <= 10
-            //            && countProducts[c] % 5 == 0
-            //            || counts[c] >= 8 && counts[c] <= 10
-            //            && countProducts[c] % 3 == 0
-            //        )
-            //        {
-            //            priceToPay -= 70;
-            //        }
-            //    }
-            //}
 
             return priceToPay;
         }
