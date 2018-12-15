093c303471bba95cb5886bc673a76678d06e3642
sbx_user1061 <sbx_user1061@ip-10-129-84-248.eu-west-2.compute.internal>
2018-12-15 17:41:52 +0000

    Sat Dec 15 17:41:52 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -31,6 +31,7 @@ namespace BeFaster.App.Solutions.CHK
             {'D', 15 },
             {'E', 40 }
         };
+
         public static Dictionary<char, int> CountProducts(this string skus)
         {
             return skus.GroupBy(c => c)
@@ -56,12 +57,28 @@ namespace BeFaster.App.Solutions.CHK
             foreach (var c in counts)
             {
                 countProducts[c.Key]++;
-                priceToPay += Prices[c.Key];
+                priceToPay = 50 * c.Key;
 
-                if (countProducts.TryGetValue('A', out var found) && found % 3 == 0)
+                if (c.Key == 'A')
                 {
-                    priceToPay -= 20;
+                    if (c.Value % 3 == 0)
+                    {
+                        priceToPay -= 20;
+                    }
+
+                    if (c.Value % 5 == 0)
+                    {
+                        priceToPay -= 50;
+                    }
                 }
+
+                //countProducts[c.Key]++;
+                //priceToPay += Prices[c.Key];
+
+                //if (countProducts.TryGetValue('A', out var found) && found % 3 == 0)
+                //{
+                //    priceToPay -= 20;
+                //}
             }
 
 
@@ -77,76 +94,76 @@ namespace BeFaster.App.Solutions.CHK
 
             //foreach (var c in arr)
             //{
-                //countProducts[c]++;
-                //priceToPay += Prices[c];
+            //countProducts[c]++;
+            //priceToPay += Prices[c];
 
-                //if (countProducts[c] % 3 == 0)
-                //{
-                //    priceToPay -= 20;
-                //}
+            //if (countProducts[c] % 3 == 0)
+            //{
+            //    priceToPay -= 20;
+            //}
 
-                //if (counts.TryGetValue('A', out var found) && found % 3 == 0)
-                //{
-                //    priceToPay -= 20;
-                //}
+            //if (counts.TryGetValue('A', out var found) && found % 3 == 0)
+            //{
+            //    priceToPay -= 20;
+            //}
 
-                //if (countProducts[c] % 5 == 0)
-                //{
-                //    priceToPay -= 50;
-                //}
+            //if (countProducts[c] % 5 == 0)
+            //{
+            //    priceToPay -= 50;
+            //}
 
-                //if (discountedProducts.ContainsKey(c))
-                //{
-                //if (counts[c] > 4
-                //    && countProducts[c] % 5 == 0
-                //    || counts[c] > 8
-                //    && countProducts[c] % 5 == 0
-                //    || counts[c] > 8
-                //    && countProducts[c] % 5 == 1)
-                //{
-                //    priceToPay -= 50;
-                //}
+            //if (discountedProducts.ContainsKey(c))
+            //{
+            //if (counts[c] > 4
+            //    && countProducts[c] % 5 == 0
+            //    || counts[c] > 8
+            //    && countProducts[c] % 5 == 0
+            //    || counts[c] > 8
+            //    && countProducts[c] % 5 == 1)
+            //{
+            //    priceToPay -= 50;
+            //}
 
-                //if (counts[c] <= 4
-                //    && countProducts[c] % 3 == 0
-                //    || counts[c] == 8
-                //    && countProducts[c] % 8 == 0)
-                //{
-                //    priceToPay -= discountedProducts[c].Discount;
-                //}
+            //if (counts[c] <= 4
+            //    && countProducts[c] % 3 == 0
+            //    || counts[c] == 8
+            //    && countProducts[c] % 8 == 0)
+            //{
+            //    priceToPay -= discountedProducts[c].Discount;
+            //}
 
-                //if (counts[c] >= 5 && counts[c] <= 7
-                //    && countProducts[c] % 5 == 0
-                //    )
-                //{
-                //    priceToPay -= 50;
-                //}
+            //if (counts[c] >= 5 && counts[c] <= 7
+            //    && countProducts[c] % 5 == 0
+            //    )
+            //{
+            //    priceToPay -= 50;
+            //}
 
-                //if (counts[c] <= 4
-                //    && countProducts[c] % 3 == 0
-                //    )
-                //{
-                //    priceToPay -= discountedProducts[c].Discount;
-                //}
+            //if (counts[c] <= 4
+            //    && countProducts[c] % 3 == 0
+            //    )
+            //{
+            //    priceToPay -= discountedProducts[c].Discount;
+            //}
 
-                //if (counts[c] >= 8 && counts[c] < 10
-                //    && countProducts[c] % 5 == 0
-                //    || counts[c] >= 8 && counts[c] < 10
-                //    && countProducts[c] % 3 == 0
-                //)
-                //{
-                //    priceToPay -= 70;
-                //}
+            //if (counts[c] >= 8 && counts[c] < 10
+            //    && countProducts[c] % 5 == 0
+            //    || counts[c] >= 8 && counts[c] < 10
+            //    && countProducts[c] % 3 == 0
+            //)
+            //{
+            //    priceToPay -= 70;
+            //}
 
-                //if (counts[c] >= 8 && counts[c] <= 10
-                //    && countProducts[c] % 5 == 0
-                //    || counts[c] >= 8 && counts[c] <= 10
-                //    && countProducts[c] % 3 == 0
-                //)
-                //{
-                //    priceToPay -= 70;
-                //}
-                //}
+            //if (counts[c] >= 8 && counts[c] <= 10
+            //    && countProducts[c] % 5 == 0
+            //    || counts[c] >= 8 && counts[c] <= 10
+            //    && countProducts[c] % 3 == 0
+            //)
+            //{
+            //    priceToPay -= 70;
+            //}
+            //}
             //}
 
             return priceToPay;
