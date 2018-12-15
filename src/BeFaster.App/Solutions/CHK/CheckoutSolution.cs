01d12dc5852a964410d72004644138fdac20901f
sbx_user1059 <sbx_user1059@ip-10-129-167-116.eu-west-2.compute.internal>
2018-12-15 13:28:03 +0000

    Sat Dec 15 13:28:03 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -76,14 +76,17 @@ namespace BeFaster.App.Solutions.CHK
                 if (discountedProducts.ContainsKey(c))
                 {
                     if (counts[c] > 4 
+                        && countProducts[c] % 5 == 0
                         || counts[c] > 9 
                         && countProducts[c] % 5 == 0)
                     {
                         priceToPay -= 50;
                     }
 
-                    if (counts[c] <= 4 && countProducts[c] % 3 == 0
-                        || counts[c] >= 8 && countProducts[c] % 8 == 0)
+                    if (counts[c] <= 4 
+                        && countProducts[c] % 3 == 0
+                        || counts[c] >= 8 
+                        && countProducts[c] % 8 == 0)
                     {
                         priceToPay -= discountedProducts[c].Discount;
                     }
