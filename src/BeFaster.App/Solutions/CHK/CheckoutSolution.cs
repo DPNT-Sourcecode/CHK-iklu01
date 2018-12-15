d002de64d7d52c799f82cd15e6eff29260cbc1e1
sbx_user1059 <sbx_user1059@ip-10-129-167-116.eu-west-2.compute.internal>
2018-12-15 14:00:58 +0000

    Sat Dec 15 14:00:58 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -91,7 +91,7 @@ namespace BeFaster.App.Solutions.CHK
                     //    priceToPay -= discountedProducts[c].Discount;
                     //}
 
-                    if (counts[c] <= 7
+                    if (counts[c] >= 5 && counts[c] <= 7
                         && countProducts[c] % 5 == 0
                         )
                     {
@@ -105,7 +105,7 @@ namespace BeFaster.App.Solutions.CHK
                         priceToPay -= discountedProducts[c].Discount;
                     }
 
-                    if (counts[c] <= 10
+                    if (counts[c] >= 8 && counts[c] <= 10
                         && countProducts[c] % 5 == 0
                         && countProducts[c] % 3 == 0
                     )
