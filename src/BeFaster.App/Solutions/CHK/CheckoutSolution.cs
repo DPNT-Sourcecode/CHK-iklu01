cac5e5c44eef3c4ffd6ca1ad5cd34d9001502731
sbx_user1060 <sbx_user1060@ip-10-128-221-238.eu-west-2.compute.internal>
2018-12-16 21:45:20 +0000

    Sun Dec 16 21:45:20 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -32,59 +32,76 @@ namespace BeFaster.App.Solutions.CHK
 
             if (skus.Contains('A') && productCounter > 2)
             {
-                priceToPay -= 20;
+                if (productCounter % 8 == 0
+                    || productCounter % 8 == 1)
+                {
+                    priceToPay -= 70;
+                }
+
+                else if (productCounter % 5 == 0
+                    || productCounter % 5 == 1
+                    || productCounter % 5 == 2)
+                {
+                    priceToPay -= 50 * (productCounter / 5);
+                }
+
+                else if (productCounter % 3 == 0
+                    || productCounter % 3 == 1)
+                {
+                    priceToPay -= 20;
+                }
             }
-                //{
-                //    if (letterCounter % 8 == 0
-                //        || letterCounter % 8 == 1)
-                //    {
-                //        priceToPay -= 70;
-                //    }
+            //{
+            //    if (letterCounter % 8 == 0
+            //        || letterCounter % 8 == 1)
+            //    {
+            //        priceToPay -= 70;
+            //    }
 
-                //    else if (letterCounter % 5 == 0
-                //        || letterCounter % 5 == 1
-                //        || letterCounter % 5 == 2)
-                //    {
-                //        priceToPay -= 50 * (letterCounter / 5);
-                //    }
+            //    else if (letterCounter % 5 == 0
+            //        || letterCounter % 5 == 1
+            //        || letterCounter % 5 == 2)
+            //    {
+            //        priceToPay -= 50 * (letterCounter / 5);
+            //    }
 
-                //    else if (letterCounter % 3 == 0
-                //        || letterCounter % 3 == 1)
-                //    {
-                //        priceToPay -= 20;
-                //    }
-                //}
+            //    else if (letterCounter % 3 == 0
+            //        || letterCounter % 3 == 1)
+            //    {
+            //        priceToPay -= 20;
+            //    }
+            //}
 
-                //if (letterCounter > 1 && skus.Contains('B'))
-                //{
-                //    priceToPay -= 30 * (letterCounter / 2);
+            //if (letterCounter > 1 && skus.Contains('B'))
+            //{
+            //    priceToPay -= 30 * (letterCounter / 2);
 
-                //    if (letterCounter % 2 == 1)
-                //    {
-                //        priceToPay -= 15 * (letterCounter / 2);
-                //    }
-                //}
+            //    if (letterCounter % 2 == 1)
+            //    {
+            //        priceToPay -= 15 * (letterCounter / 2);
+            //    }
+            //}
 
-                //else if (letterCounter > 1)
-                //{
-                //    if (letterCounter % 2 == 0 
-                //        || letterCounter % 2 == 1)
-                //    {
-                //        priceToPay -= 15 * (letterCounter / 2);
-                //    }
-                //}
+            //else if (letterCounter > 1)
+            //{
+            //    if (letterCounter % 2 == 0 
+            //        || letterCounter % 2 == 1)
+            //    {
+            //        priceToPay -= 15 * (letterCounter / 2);
+            //    }
+            //}
 
-                //if (letterCounter > 2 && skus.Contains('F'))
-                //{
-                //    if (letterCounter % 3 == 0 
-                //        || letterCounter % 3 == 1 
-                //        || letterCounter % 3 == 2)
-                //    {
-                //        priceToPay -= 10 * (letterCounter / 3);
-                //    }
-                //}
+            //if (letterCounter > 2 && skus.Contains('F'))
+            //{
+            //    if (letterCounter % 3 == 0 
+            //        || letterCounter % 3 == 1 
+            //        || letterCounter % 3 == 2)
+            //    {
+            //        priceToPay -= 10 * (letterCounter / 3);
+            //    }
+            //}
 
-                return priceToPay;
+            return priceToPay;
         }
 
         public static int Checkout(string skus)
