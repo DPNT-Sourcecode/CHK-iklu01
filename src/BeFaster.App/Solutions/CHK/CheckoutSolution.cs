6a6d869fb0e61d6a1b7b1a1da33e4968ead7849b
sbx_user1060 <sbx_user1060@ip-10-128-221-238.eu-west-2.compute.internal>
2018-12-16 20:44:54 +0000

    Sun Dec 16 20:44:54 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,90 +1,18 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
-        //public static Dictionary<char, int> Prices => new Dictionary<char, int>
-        //{
-        //    {'A', 50 },
-        //    {'B', 30 },
-        //    {'C', 20 },
-        //    {'D', 15 },
-        //    {'E', 40 },
-        //    {'F', 10 }
-        //};
-
         public static int GetProduct(string skus)
         {
             var priceToPay = 0;
             var skusToCharacter = skus.ToCharArray(0, skus.Length);
-            var increaseProductNumber = new Dictionary<char, int>();
-            var prices = new Dictionary<char, int>();
-
-            for (var product = 'A'; product <= 'Z'; product++)
-            {
-                increaseProductNumber[product] = 0;
-            }
-
-            for (var product = 'A'; product <= 'Z'; product++)
-            {
-                switch (product)
-                {
-                    case 'F':
-                    case 'H':
-                    case 'O':
-                    case 'Y':
-                        prices[product] = 10;
-                        break;
-                    case 'D':
-                    case 'M':
-                        prices[product] = 15;
-                        break;
-                    case 'C':
-                    case 'G':
-                    case 'T':
-                    case 'W':
-                        prices[product] = 20;
-                        break;
-                    case 'B':
-                    case 'Q':
-                    case 'S':
-                        prices[product] = 30;
-                        break;
-                    case 'I':
-                        prices[product] = 35;
-                        break;
-                    case 'E':
-                    case 'N':
-                    case 'U':
-                        prices[product] = 40;
-                        break;
-                    case 'A':
-                    case 'P':
-                    case 'R':
-                    case 'V':
-                    case 'Z':
-                        prices[product] = 50;
-                        break;
-                    case 'J':
-                        prices[product] = 60;
-                        break;
-                    case 'K':
-                        prices[product] = 80;
-                        break;
-                    case 'L':
-                    case 'X':
-                        prices[product] = 90;
-                        break;
-                }
-            }
 
             foreach (var charachter in skusToCharacter)
             {
-                increaseProductNumber[charachter]++;
-                priceToPay += prices[charachter];
+                Product.ProductNumber[charachter]++;
+                priceToPay += Product.ProductPrice[charachter];
             }
 
             //if (letterCounter > 2)
