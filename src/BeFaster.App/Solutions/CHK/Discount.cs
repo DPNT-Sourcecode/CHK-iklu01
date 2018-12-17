3dbdd0274ca203a8b50dd645276c6b73001e689c
sbx_user1059 <sbx_user1059@ip-10-129-24-80.eu-west-2.compute.internal>
2018-12-17 16:17:50 +0000

    Mon Dec 17 16:17:50 UTC 2018

--- src/BeFaster.App/Solutions/CHK/Discount.cs
+++ src/BeFaster.App/Solutions/CHK/Discount.cs
@@ -42,90 +42,93 @@ namespace BeFaster.App.Solutions.CHK
 
             foreach (var count in counts)
             {
-                if (count.Key == 'A' && count.Value > 2)
+                switch (count.Key)
                 {
-                    if (count.Value % 8 <= 1)
-                    {
-                        priceToPay -= 70;
-                    }
-
-                    else if (count.Value % 5 <= 2)
-                    {
-                        priceToPay -= 50 * (count.Value / 5);
-                    }
-
-                    else if (count.Value % 3 <= 1)
-                    {
-                        priceToPay -= 20;
-                    }
-                }
-                else if (count.Key == 'F' && count.Value > 2)
-                {
-                    if (count.Value % 3 <= 2)
-                    {
-                        priceToPay -= 10 * (count.Value / 3);
-                    }
-                }
-                else if (count.Key == 'H' && count.Value > 4)
-                {
-                    if (count.Value >= 10)
-                    {
-                        if (count.Value % 5 == 0)
+                    case 'A' when count.Value > 2:
+                        if (count.Value % 8 <= 1)
                         {
-                            priceToPay -= 10 * (count.Value / 5);
+                            priceToPay -= 70;
                         }
-                    }
 
-                    else if (count.Value % 5 <= 4)
-                    {
-                        priceToPay -= 5;
-                    }
-                }
-                else if (count.Key == 'K' && count.Value > 1)
-                {
-                    if (count.Value % 2 <= 1)
-                    {
-                        priceToPay -= 10 * (count.Value / 2);
-                    }
-                }
-                else if (count.Key == 'P' && count.Value > 4)
-                {
-                    if (count.Value % 5 <= 4)
-                    {
-                        priceToPay -= 50 * (count.Value / 5);
-                    }
-                }
-                else if (count.Key == 'Q' && count.Value > 2)
-                {
-                    if (count.Value % 3 <= 2)
-                    {
-                        priceToPay -= 10;
-                    }
-                }
-                else if (count.Key == 'V' && count.Value > 1)
-                {
-                    if (count.Value % 3 <= 2)
-                    {
-                        priceToPay -= 20;
-                    }
-                    else if (count.Value % 2 <= 1)
-                    {
-                        priceToPay -= 10;
-                    }
-                }
-                else if (count.Key == 'U' && count.Value > 3)
-                {
-                    if (count.Value % 4 <= 3)
-                    {
-                        priceToPay -= 40 * (count.Value / 4);
-                    }
-                }
-                else if (count.Key == 'B' && count.Value > 1)
-                {
-                    if (count.Value % 2 <= 1)
-                    {
-                        priceToPay -= 15 * (count.Value / 2);
-                    }
+                        else if (count.Value % 5 <= 2)
+                        {
+                            priceToPay -= 50 * (count.Value / 5);
+                        }
+
+                        else if (count.Value % 3 <= 1)
+                        {
+                            priceToPay -= 20;
+                        }
+
+                        break;
+                    case 'B' when count.Value > 1:
+                        if (count.Value % 2 <= 1)
+                        {
+                            priceToPay -= 15 * (count.Value / 2);
+                        }
+
+                        break;
+                    case 'F' when count.Value > 2:
+                        if (count.Value % 3 <= 2)
+                        {
+                            priceToPay -= 10 * (count.Value / 3);
+                        }
+
+                        break;
+                    case 'H' when count.Value > 4:
+                        if (count.Value >= 10)
+                        {
+                            if (count.Value % 5 == 0)
+                            {
+                                priceToPay -= 10 * (count.Value / 5);
+                            }
+                        }
+
+                        else if (count.Value % 5 <= 4)
+                        {
+                            priceToPay -= 5;
+                        }
+
+                        break;
+                    case 'K' when count.Value > 1:
+                        if (count.Value % 2 <= 1)
+                        {
+                            priceToPay -= 10 * (count.Value / 2);
+                        }
+
+                        break;
+                    case 'P' when count.Value > 4:
+                        if (count.Value % 5 <= 4)
+                        {
+                            priceToPay -= 50 * (count.Value / 5);
+                        }
+
+                        break;
+                    case 'Q' when count.Value > 2:
+                        if (count.Value % 3 <= 2)
+                        {
+                            priceToPay -= 10;
+                        }
+
+                        break;
+                    case 'U' when count.Value > 3:
+                        if (count.Value % 4 <= 3)
+                        {
+                            priceToPay -= 40 * (count.Value / 4);
+                        }
+
+                        break;
+                    case 'V' when count.Value > 1:
+                        if (count.Value % 3 <= 2)
+                        {
+                            priceToPay -= 20;
+                        }
+                        else if (count.Value % 2 <= 1)
+                        {
+                            priceToPay -= 10;
+                        }
+
+                        break;
                 }
             }
 
