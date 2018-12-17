16eebac6a99fe20914709817cde40404eac34a22
sbx_user1059 <sbx_user1059@ip-10-129-95-224.eu-west-2.compute.internal>
2018-12-17 12:06:42 +0000

    Mon Dec 17 12:06:42 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -9,6 +9,11 @@ namespace BeFaster.App.Solutions.CHK
         public static Dictionary<char, int> CountProducts(this string skus) => skus.GroupBy(c => c)
             .ToDictionary(group => group.Key, group => group.Count());
 
+        public static bool ContainsLetter(string skus)
+        {
+            return skus.IndexOfAny("ABEFHKNPQRUV".ToCharArray()) != -1;
+        }
+
         public static int GetProduct(string skus)
         {
             Product.AddProductNumer();
@@ -17,7 +22,7 @@ namespace BeFaster.App.Solutions.CHK
             var priceToPay = 0;
             var counts = skus.CountProducts();
             var skusToCharacter = skus.ToCharArray(0, skus.Length);
-            var productCounter = 0;
+            //var productCounter = 0;
 
             foreach (var charachter in skusToCharacter)
             {
@@ -25,114 +30,114 @@ namespace BeFaster.App.Solutions.CHK
                 priceToPay += Product.ProductPrice[charachter];
             }
 
-            foreach (var count in counts)
-            {
-                productCounter += skus.Count(x => x == count.Key);
-            }
+            //foreach (var count in counts)
+            //{
+            //    productCounter += skus.Count(x => x == count.Key);
+            //}
 
-            if (skus.Contains('A') && productCounter > 2)
-            {
-                if (productCounter % 8 <= 1)
-                {
-                    priceToPay -= 70;
-                }
+            //if (skus.Contains('A') && productCounter > 2)
+            //{
+            //    if (productCounter % 8 <= 1)
+            //    {
+            //        priceToPay -= 70;
+            //    }
 
-                else if (productCounter % 5 <= 2)
-                {
-                    priceToPay -= 50 * (productCounter / 5);
-                }
+            //    else if (productCounter % 5 <= 2)
+            //    {
+            //        priceToPay -= 50 * (productCounter / 5);
+            //    }
 
-                else if (productCounter % 3 <= 1)
-                {
-                    priceToPay -= 20;
-                }
-            }
+            //    else if (productCounter % 3 <= 1)
+            //    {
+            //        priceToPay -= 20;
+            //    }
+            //}
 
-            if (Product.ProductNumber['E'] > 1 && skus.Contains('B'))
-            {
-                priceToPay -= 30 * (Product.ProductNumber['E'] / 2);
+            //if (Product.ProductNumber['E'] > 1 && skus.Contains('B'))
+            //{
+            //    priceToPay -= 30 * (Product.ProductNumber['E'] / 2);
 
-                if (Product.ProductNumber['B'] % 2 == 1)
-                {
-                    priceToPay -= 15 * (Product.ProductNumber['B'] / 2);
-                }
-            }
+            //    if (Product.ProductNumber['B'] % 2 == 1)
+            //    {
+            //        priceToPay -= 15 * (Product.ProductNumber['B'] / 2);
+            //    }
+            //}
 
-            if (skus.Contains('F') && productCounter > 2)
-            {
-                if (productCounter % 3 <= 2)
-                {
-                    priceToPay -= 10 * (productCounter / 3);
-                }
-            }
+            //if (skus.Contains('F') && productCounter > 2)
+            //{
+            //    if (productCounter % 3 <= 2)
+            //    {
+            //        priceToPay -= 10 * (productCounter / 3);
+            //    }
+            //}
 
-            if (skus.Contains('H') && productCounter > 4)
-            {
-                if (productCounter >= 10)
-                {
-                    if (productCounter % 5 == 0)
-                    {
-                        priceToPay -= 10 * (productCounter / 5);
-                    }
-                }
+            //if (skus.Contains('H') && productCounter > 4)
+            //{
+            //    if (productCounter >= 10)
+            //    {
+            //        if (productCounter % 5 == 0)
+            //        {
+            //            priceToPay -= 10 * (productCounter / 5);
+            //        }
+            //    }
 
-                else if (productCounter % 5 <= 4)
-                {
-                    priceToPay -= 5;
-                }
-            }
+            //    else if (productCounter % 5 <= 4)
+            //    {
+            //        priceToPay -= 5;
+            //    }
+            //}
 
-            if (skus.Contains('K') && productCounter > 1)
-            {
-                if (productCounter % 2 <= 1)
-                {
-                    priceToPay -= 10 * (productCounter / 2);
-                }
-            }
+            //if (skus.Contains('K') && productCounter > 1)
+            //{
+            //    if (productCounter % 2 <= 1)
+            //    {
+            //        priceToPay -= 10 * (productCounter / 2);
+            //    }
+            //}
 
-            if (skus.Contains('P') && productCounter > 4)
-            {
-                if (productCounter % 5 <= 4)
-                {
-                    priceToPay -= 50 * (productCounter / 5);
-                }
-            }
+            //if (skus.Contains('P') && productCounter > 4)
+            //{
+            //    if (productCounter % 5 <= 4)
+            //    {
+            //        priceToPay -= 50 * (productCounter / 5);
+            //    }
+            //}
 
-            if (skus.Contains('Q') && productCounter > 2)
-            {
-                if (productCounter % 3 <= 2)
-                {
-                    priceToPay -= 10;
-                }
-            }
+            //if (skus.Contains('Q') && productCounter > 2)
+            //{
+            //    if (productCounter % 3 <= 2)
+            //    {
+            //        priceToPay -= 10;
+            //    }
+            //}
 
-            if (skus.Contains('V') && productCounter > 1)
-            {
-                if (productCounter % 3 <= 2)
-                {
-                    priceToPay -= 20;
-                }
-                else if (productCounter % 2 <= 1)
-                {
-                    priceToPay -= 10;
-                }
-            }
+            //if (skus.Contains('V') && productCounter > 1)
+            //{
+            //    if (productCounter % 3 <= 2)
+            //    {
+            //        priceToPay -= 20;
+            //    }
+            //    else if (productCounter % 2 <= 1)
+            //    {
+            //        priceToPay -= 10;
+            //    }
+            //}
 
-            if (skus.Contains('U') && productCounter > 3)
-            {
-                if (productCounter % 4 <= 3)
-                {
-                    priceToPay -= 40 * (productCounter / 4);
-                }
-            }
+            //if (skus.Contains('U') && productCounter > 3)
+            //{
+            //    if (productCounter % 4 <= 3)
+            //    {
+            //        priceToPay -= 40 * (productCounter / 4);
+            //    }
+            //}
 
-            if (skus.Contains('B') && productCounter > 1)
-            {
-                if (productCounter % 2 <= 1)
-                {
-                    priceToPay -= 15 * (productCounter / 2);
-                }
-            }
+            //if (skus.Contains('B') && productCounter > 1)
+            //{
+            //    if (productCounter % 2 <= 1)
+            //    {
+            //        priceToPay -= 15 * (productCounter / 2);
+            //    }
+            //}
 
             return priceToPay;
         }
