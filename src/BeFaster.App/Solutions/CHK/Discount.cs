417eb43d9a87e93ebd857a10ec8d02af33074484
sbx_user1059 <sbx_user1059@ip-10-128-248-121.eu-west-2.compute.internal>
2018-12-21 20:38:10 +0000

    Fri Dec 21 20:38:10 UTC 2018

--- src/BeFaster.App/Solutions/CHK/Discount.cs
+++ src/BeFaster.App/Solutions/CHK/Discount.cs
@@ -5,6 +5,9 @@ namespace BeFaster.App.Solutions.CHK
 {
     public static class Discount
     {
+        //public static Dictionary<char, int> ProductNumber = new Dictionary<char, int>();
+        //public static Dictionary<char, int> ProductPrice = new Dictionary<char, int>();
+
         public static Dictionary<char, int> CountProducts(this string skus)
         {
             return skus.GroupBy(c => c).ToDictionary(group => group.Key, group => group.Count());
@@ -20,56 +23,46 @@ namespace BeFaster.App.Solutions.CHK
             foreach (var product in skusToCharacter)
             {
                 Product.ProductNumber[product]++;
-                priceToPay = 0;
 
-                switch (product)
+                if (product == 'F' || product == 'H' || product == 'O' || product == 'Y')
                 {
-                    case 'F':
-                    case 'H':
-                    case 'O':
-                    case 'Y':
-                        priceToPay += 10 * product;
-                        break;
-                    case 'D':
-                    case 'M':
-                        priceToPay += 15 * product;
-                        break;
-                    case 'C':
-                    case 'G':
-                    case 'T':
-                    case 'W':
-                        priceToPay += 20 * product;
-                        break;
-                    case 'B':
-                    case 'Q':
-                    case 'S':
-                        priceToPay += 30 * product;
-                        break;
-                    case 'I':
-                        priceToPay += 35 * product;
-                        break;
-                    case 'E':
-                    case 'N':
-                    case 'U':
-                        priceToPay += 40 * product;
-                        break;
-                    case 'A':
-                    case 'P':
-                    case 'R':
-                    case 'V':
-                    case 'Z':
-                        priceToPay += 50 * product;
-                        break;
-                    case 'J':
-                        priceToPay += 60 * product;
-                        break;
-                    case 'K':
-                        priceToPay += 80 * product;
-                        break;
-                    case 'L':
-                    case 'X':
-                        priceToPay += 90 * product;
-                        break;
+                    priceToPay += 10 * Product.ProductPrice[product];
+                }
+                else if (product == 'D' || product == 'M')
+                {
+                    priceToPay += 15 * Product.ProductPrice[product];
+                }
+                else if (product == 'C' || product == 'G' || product == 'T' || product == 'W')
+                {
+                    priceToPay += 20 * Product.ProductPrice[product];
+                }
+                else if (product == 'B' || product == 'Q' || product == 'S')
+                {
+                    priceToPay += 30 * Product.ProductPrice[product];
+                }
+                else if (product == 'I')
+                {
+                    priceToPay += 35 * Product.ProductPrice[product];
+                }
+                else if (product == 'E' || product == 'N' || product == 'U')
+                {
+                    priceToPay += 40 * Product.ProductPrice[product];
+                }
+                else if (product == 'A' || product == 'P' || product == 'R' || product == 'V' || product == 'Z')
+                {
+                    priceToPay += 50 * Product.ProductPrice[product];
+                }
+                else if (product == 'J')
+                {
+                    priceToPay += 60 * Product.ProductPrice[product];
+                }
+                else if (product == 'K')
+                {
+                    priceToPay += 80 * Product.ProductPrice[product];
+                }
+                else if (product == 'L' || product == 'X')
+                {
+                    priceToPay += 90 * Product.ProductPrice[product];
                 }
             }
 
