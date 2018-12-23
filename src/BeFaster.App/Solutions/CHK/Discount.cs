ff2189539bbbdea50b280b17b0c0d14429073bec
sbx_user1060 <sbx_user1060@ip-10-128-210-192.eu-west-2.compute.internal>
2018-12-23 21:30:22 +0000

    Sun Dec 23 21:30:22 UTC 2018

--- src/BeFaster.App/Solutions/CHK/Discount.cs
+++ src/BeFaster.App/Solutions/CHK/Discount.cs
@@ -28,15 +28,24 @@ namespace BeFaster.App.Solutions.CHK
             int counterB = 0, counterE = 0, counterN = 0, counterQ = 0, counterR = 0, counterS = 0,
                 counterT = 0, counterX = 0, counterY = 0, counterZ = 0, counterSpecial = 0;
             var lowestPrice = 21;
+            var secondLowestPrice = 21;
 
             foreach (var product in products)
             {
                 Product.ProductNumber[product]++;
                 priceToPay += Product.ProductPrice[product];
 
-                if (Regex.IsMatch(product.ToString(), @"[STXYZ]") && Product.ProductPrice[product] < lowestPrice)
+                if (Regex.IsMatch(product.ToString(), @"[STXYZ]"))
                 {
-                    lowestPrice = Product.ProductPrice[product];
+                    if (Product.ProductPrice[product] < lowestPrice)
+                    {
+                        lowestPrice = Product.ProductPrice[product];
+                    }
+                    if (Product.ProductPrice[product] < lowestPrice
+                        && secondLowestPrice > lowestPrice)
+                    {
+                        secondLowestPrice = Product.ProductPrice[product];
+                    }
                 }
 
                 switch (product)
@@ -65,6 +74,10 @@ namespace BeFaster.App.Solutions.CHK
                     specialPrice = 45 * (counterSpecial / 3);
                 }
                 else if (counterSpecial % 3 <= 2)
+                {
+                    specialPrice = 45 * (counterSpecial / 3) + lowestPrice + secondLowestPrice;
+                }
+                else if (counterSpecial % 3 <= 1)
                 {
                     specialPrice = 45 * (counterSpecial / 3) + lowestPrice;
                 }
